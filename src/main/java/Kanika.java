3fb2f671b4ea1477dba9569cbecbe7513ee5dde6
namit26 <namit.sharma.eee13@itbhu.ac.in>
2017-09-17 15:46:44 +0530

    added a new function

--- src/main/java/Kanika.java
+++ src/main/java/Kanika.java
@@ -1,5 +1,9 @@
 public class Kanika {
     public static void main(String[] args) {
+
         System.out.println("My name is kanika");
+    }
+    public static void aNewFunction(){
+        System.out.println("This is a new function");
     }
 }
