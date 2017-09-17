81e7cd43877ffb8e8e2fd356bed4aa88f69a953e
namit26 <namit.sharma.eee13@itbhu.ac.in>
2017-09-17 15:56:44 +0530

    variable a added

--- src/main/java/Namit.java
+++ src/main/java/Namit.java
@@ -1,5 +1,8 @@
 public class Namit {
     public static void main(String[] args) {
         System.out.println("My name is Namit");
+        int a = 0;
+        a= 1+2;
+        System.out.println(a);
     }
 }
